913d0c0c1a7799757024089cee531ea152acd5d8
Lee Yeonggeun <dudrms9397@naver.com>
2024-01-15 12:08:31 +0900

    ch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -64,7 +64,7 @@ function App() {
               onChange={changeHandler}
               value={value}
             />
-            <button type="submit">확인.</button>
+            <button type="submit">확인</button>
           </form>
         </div>
       </header>
